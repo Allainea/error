4cfda22e78ffd81ab02cf3428f348f276a7e3214
Allainea <127365242+Allainea@users.noreply.github.com>
2023-07-06 19:45:14 +0800

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\SalesController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return redirect('/admin');
+});
+
+Auth::routes();
+
+Route::prefix('admin')->middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/settings', [SettingController::class, 'index'])->name('settings.index');
+    Route::post('/settings', [SettingController::class, 'store'])->name('settings.store');
+    Route::resource('products', ProductController::class);
+    Route::resource('customers', CustomerController::class);
+    Route::resource('orders', OrderController::class);
+
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
+    Route::post('/cart/change-qty', [CartController::class, 'changeQty']);
+    Route::delete('/cart/delete', [CartController::class, 'delete']);
+    Route::delete('/cart/empty', [CartController::class, 'empty']);
+
+    Route::get('admin/orders/{order}/print', [OrderController::class, 'print'])->name('orders.print');
+    Route::get('/reports/weekly-sales', [SalesReportController::class, 'weeklySales'])->name('weekly-sales-report.index');
+    Route::get('/reports/monthly-sales', [SalesReportController::class, 'monthlySales'])->name('monthly-sales-report.index');
+    Route::get('/sales-report', [SalesReportController::class, 'index'])->name('sales-report.index');
+
+    Route::get('/admin/report', [OrderController::class, 'report'])->name('report.index');
+    Route::get('/orders/printAll', [OrderController::class, 'printAll'])->name('orders.printAll');
+    Route::get('/admin/orders/export', [OrderController::class, 'export'])->name('orders.export');
+
+
+
+
+
+});
