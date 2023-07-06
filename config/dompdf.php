f3d65b7a487c4e143efc3d15c948cd064b6c7cc5
Allainea <127365242+Allainea@users.noreply.github.com>
2023-07-06 20:18:27 +0800

    Delete config directory

--- config/dompdf.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'temp_dir' => storage_path('app/dompdf'), // Set a temporary directory for Dompdf
-    'font_dir' => storage_path('fonts/'), // Set the directory where your fonts are stored
-    'font_cache' => storage_path('app/dompdf/fontcache'), // Set a cache directory for Dompdf fonts
-    'chroot' => base_path(), // Set the base path for Dompdf
-    'default_font' => 'Arial', // Set the default font for Dompdf
-];
